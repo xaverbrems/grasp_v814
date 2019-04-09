db4361b2871641bef28438b0464e1fdde9452ca4
xbrems <xbrems@STRESSIOPC10.office.frm2>
2019-04-09 16:19:39 +0200

    changed the order in which grasp_plot and grasp_plot3 are added to the matlab search path: this allows to have pseudo_fn.m in both folders since matlab at first searches for pseudo_fn.m within grasp_plot. pseudo_fn.m is added to grasp_plot3 again

---
+++ grasp_m_barebones/grasp_plot3/pseudo_fn.m
@@ -0,0 +1,145 @@
+function y_out=pseudo_fn(x_in,parameters_in,fitdata)
+
+
+if nargin <3; fitdata = []; end
+
+global graspplot
+
+%Evaluate the function
+%Defaults if no smearing
+finesse = 1;
+x_smear = x_in;
+x_weight = ones(size(x_smear));
+
+%q-resolution data can be sent as:
+%fitdata.hdat_all - combined overall q resolution, delta_q(q)
+%AND / OR
+%fitdata.resolution_kernels_all
+%with fields
+%   .lambda.sigma  .shape
+%   .theta.sigma .shape
+%   .pixel.sigma .shape
+%   .binning.sigma .shape
+%   .x {cell} - cells of lines of x-coordinates corresponding to the convolution kernel
+%   .weight {cell} - cells of lines of weights corresponding to the convolution kernel
+%   .sigma - combined overall q resolution, delta_q(q).  Same as in fitdata.hdat_all
+
+% %Plot Resolutions
+plot_res = 'off';
+if strcmp(plot_res,'on'); figure; end
+
+%***** Instrument Resolution Convolution Control *****
+if graspplot.fitter.include_res_check ==1  %Convolute instrument resolution if availaible
+    
+    if not(isfield(fitdata,'resolution_kernels_all')); fitdata.resolution_kernels_all = []; end
+    
+    if not(isempty(fitdata.resolution_kernels_all)) %Full resolution kernels exist - use this in preference
+        %disp('Convoluting to pre-built resolution kernel in pseudo_fn.m')
+        if not(isempty(fitdata.resolution_kernels_all))
+            
+            finesse = length(fitdata.resolution_kernels_all.x{1}); %Find out what finesse was used in caluclating the kernels
+            x_smear = []; x_weight = [];
+            for n = 1:length(fitdata.resolution_kernels_all.x)
+                %Kernels are already build, just need to string them together into a list
+                x_smear = [x_smear; rot90(fitdata.resolution_kernels_all.x{n},3)];
+                x_weight = [x_weight; rot90(fitdata.resolution_kernels_all.weight{n},3)];
+                
+                if strcmp(plot_res,'on')
+                    if n == 1 || n==round(length(fitdata.resolution_kernels_all.x)/2) || n==length(fitdata.resolution_kernels_all.x)
+                        hold on
+                        plot(fitdata.resolution_kernels_all.x{n},fitdata.resolution_kernels_all.weight{n},'.-w')
+                    end
+                end
+            end
+        end
+        
+        
+    elseif isfield(fitdata,'hdat_all') %Convolute to Gaussian resolution if full shape kernel no availaible
+        
+        %disp('Convoluting to Instrument resolution with Gaussian Kernel in pseudo_fn.m');
+        if not(isempty(fitdata.hdat_all))
+            disp('No full shape kernel availaible in: pseudo_fn.m');
+            disp('using hdat to generate gaussian kernal in pseudo_fn.m');
+            %Run the model below for a range of q's weighted by a distribution describing the resolution
+            %Build all the new x-matricies
+            x_smear = []; x_weight = [];
+            
+            fwhmwidth = graspplot.resolution_control.fwhmwidth;
+            finesse = graspplot.resolution_control.finesse * graspplot.resolution_control.fwhmwidth;
+            for n = 1:length(x_in)
+                
+                %Make x coordinate range in high resolution for each x_in point
+                low = x_in(n)-fwhmwidth*fitdata.hdat_all(n); high = x_in(n)+fwhmwidth*fitdata.hdat_all(n);
+                dx = (high -low) /(finesse-1); %Make sure there is an ODD number of points
+                new_x = low:dx:high; %Kernel x-range in higher resolution than the original data
+ 
+                %Convolution function
+                [weight] = gauss_kernel_1d(x_in(n),fitdata.hdat_all(n)/(2*sqrt(2*log(2))),1,new_x); %Arguments (xcentre,sigma_x,intint,x_range,dx)
+                weight = weight*dx;
+                
+                %Plot resolution
+                if strcmp(plot_res,'on')
+                    if n ==1 || n == length(x_in)
+                        hold on
+                        plot(new_x,weight,'-m.');
+                    end
+                end
+                
+                %assemble a new x_list
+                x_smear = [x_smear; rot90(new_x,3)];
+                x_weight = [x_weight; rot90(weight,3)];
+            end
+        end
+    else
+        %disp('No Instrument Resolution information found')
+    end
+end
+
+
+%The above produces a new x-list to run through the function
+x = x_smear; %Replace variable name as all functions expect 'x'
+
+
+%Multiplex control
+param_number = 1;
+for fn_multiplex = 1:graspplot.fitter.number1d
+    %Prepare the variables from the parameters
+    for variable_loop = 1:graspplot.fitter.function_info_1d.no_parameters
+        
+        %check for grouped parameters
+        if graspplot.fitter.function_info_1d.group(param_number) == 1
+            %parameter is grouped - copy the first copy of this parameter to this position
+            parameters_in(param_number) = parameters_in(variable_loop);
+        end
+        eval([graspplot.fitter.function_info_1d.variable_names{param_number} ' = ' num2str(parameters_in(param_number)) ';']);
+        param_number = param_number+1;
+    end
+    for line = 1:length(graspplot.fitter.function_info_1d.math_code)
+        %graspplot.fitter.function_info_1d.math_code{line}
+        eval(graspplot.fitter.function_info_1d.math_code{line});%this takes 'x' and gives a variable called 'y' as the result
+    end
+    
+    if fn_multiplex ==1
+        y_multiplex = y;
+    else
+        y_multiplex = y_multiplex +y;
+    end
+end
+
+%figure
+%plot(x,y_multiplex,'.')
+
+%The final y_multiplex value here is actually too long in the case of smearing an needs weighted averaging
+%Final Weighted average smearing
+y_out = [];
+counter = 1;
+for n =1:length(x_in)
+    y_out(n) = 0;
+%    x_out(n) = 0;
+    for m = 1:finesse
+        if counter > length(y_multiplex); break; end %Trying to error trap Poly Sphere model with poly = 0
+        y_out(n) = y_out(n) + y_multiplex(counter)*x_weight(counter); %The smeared intensity
+        counter = counter +1;
+    end
+end
+y_out = rot90(y_out,3);
