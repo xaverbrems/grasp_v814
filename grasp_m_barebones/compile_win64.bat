a38b053adfaa2accf58ebe726d84c3b149f85e68
xbrems <xbrems@STRESSIOPC10.office.frm2>
2019-04-18 13:32:45 +0200

    modified batch file to generate standalone grasp.exe

--- grasp_m_barebones/compile_win64.bat
+++ grasp_m_barebones/compile_win64.bat
@@ -1,12 +1,12 @@
 
 
-copy f:\Matlab\grasp_m_barebones\instrument_ini\det_efficiency\ f:\Matlab\grasp_barebones_compiled\grasp_windows_compiled64\instrument_ini\det_efficiency\
-copy f:\Matlab\grasp_m_barebones\instrument_ini\*.ini f:\Matlab\grasp_barebones_compiled\grasp_windows_compiled64\instrument_ini\
-copy f:\Matlab\grasp_m_barebones\grasp_script\scripts\*.m f:\Matlab\grasp_barebones_compiled\grasp_windows_compiled64\grasp_script\
-copy f:\Matlab\grasp_m_barebones\grasp_script\gs_help.pdf f:\Matlab\grasp_barebones_compiled\grasp_windows_compiled64\grasp_script\
-
-matlab -r "cd f:\Matlab\grasp_m_barebones\; grasp_startup;cd f:\Matlab\grasp_barebones_compiled\grasp_windows_compiled64\;tic;mcc -m -v grasp; toc"
+xcopy H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_m_barebones\instrument_ini\det_efficiency\ H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_barebones_compiled\grasp_windows_compiled64\instrument_ini\det_efficiency\
+xcopy H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_m_barebones\instrument_ini\*.ini H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_barebones_compiled\grasp_windows_compiled64\instrument_ini\
+xcopy H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_m_barebones\grasp_script\scripts\*.m H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_barebones_compiled\grasp_windows_compiled64\grasp_script\
+xcopy H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_m_barebones\grasp_script\gs_help.pdf H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_barebones_compiled\grasp_windows_compiled64\grasp_script\
+xcopy H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_m_barebones\ini\*.ini H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_barebones_compiled\grasp_windows_compiled64\ini\
+matlab -r "cd H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_m_barebones; grasp_startup; cd H:\Werkstudent\Grasp_Modifikation\Grasp_v814\grasp_barebones_compiled\grasp_windows_compiled64\;tic;mcc -m -v grasp; toc"
 
 pause
 
-"c:\Program Files\7-Zip\7z.exe" a -r "e:\ILL Cloud\Grasp\barebones\grasp_barebones_win64_R2018b.zip" f:\Matlab\grasp_barebones_compiled\grasp_windows_compiled64\*.*
+
