9d9acb947a74f4b44e81dcfcd12384bac23aea1d
xbrems <xbrems@STRESSIOPC10.office.frm2>
2019-06-28 13:13:11 +0200

    adapted positions of the parameter fields so it is more symmetric

--- grasp_m_barebones/user_modules/bayes_window.m
+++ grasp_m_barebones/user_modules/bayes_window.m
@@ -29,19 +29,19 @@ figure_handle = grasp_handles.window_modules.bayes.window;
 
 %input name/label
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.90 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','Label:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.input_name = uicontrol(figure_handle,'units','normalized','Position',[0.37 0.93 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.input_name),'HorizontalAlignment','right','Tag','input_name','Visible','on','callback','bayes_callbacks(''input_name'');');
+grasp_handles.user_modules.bayes.input_name = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.93 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.input_name),'HorizontalAlignment','right','Tag','input_name','Visible','on','callback','bayes_callbacks(''input_name'');');
 
 %input index
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.85 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','input index:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.input_index = uicontrol(figure_handle,'units','normalized','Position',[0.37 0.85 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.input_index),'HorizontalAlignment','right','Tag','input_index','Visible','on','callback','bayes_callbacks(''input_index'');');
+grasp_handles.user_modules.bayes.input_index = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.85 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.input_index),'HorizontalAlignment','right','Tag','input_index','Visible','on','callback','bayes_callbacks(''input_index'');');
 
 %output index
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.75 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','output index:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.output_index = uicontrol(figure_handle,'units','normalized','Position',[0.37 0.75 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.output_index),'HorizontalAlignment','right','Tag','output_index','Visible','on','callback','bayes_callbacks(''output_index'');');
+grasp_handles.user_modules.bayes.output_index = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.75 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.output_index),'HorizontalAlignment','right','Tag','output_index','Visible','on','callback','bayes_callbacks(''output_index'');');
 
 %spot x
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.65 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','Spot x:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.spot_x = uicontrol(figure_handle,'units','normalized','Position',[0.32 0.65 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.spot_x),'HorizontalAlignment','right','Tag','spot_x','Visible','on','callback','bayes_callbacks(''spot_x'');');
+grasp_handles.user_modules.bayes.spot_x = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.65 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.spot_x),'HorizontalAlignment','right','Tag','spot_x','Visible','on','callback','bayes_callbacks(''spot_x'');');
 
 %spot y
 uicontrol(figure_handle,'units','normalized','Position',[0.55 0.65 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','Spot y:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
@@ -49,16 +49,16 @@ grasp_handles.user_modules.bayes.spot_y = uicontrol(figure_handle,'units','norma
 
 %eta0
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.55 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','eta 0:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.eta0 = uicontrol(figure_handle,'units','normalized','Position',[0.32 0.55 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.eta0),'HorizontalAlignment','right','Tag','eta0','Visible','on','callback','bayes_callbacks(''eta0'');');
+grasp_handles.user_modules.bayes.eta0 = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.55 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.eta0),'HorizontalAlignment','right','Tag','eta0','Visible','on','callback','bayes_callbacks(''eta0'');');
 
 
 %san offset
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.45 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','san offset:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.sanoffset = uicontrol(figure_handle,'units','normalized','Position',[0.32 0.45 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.sanoffset),'HorizontalAlignment','right','Tag','sanoffset','Visible','on','callback','bayes_callbacks(''sanoffset'');');
+grasp_handles.user_modules.bayes.sanoffset = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.45 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.sanoffset),'HorizontalAlignment','right','Tag','sanoffset','Visible','on','callback','bayes_callbacks(''sanoffset'');');
 
 %phi offset
 uicontrol(figure_handle,'units','normalized','Position',[0.05 0.35 0.4 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','phi offset:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.phioffset = uicontrol(figure_handle,'units','normalized','Position',[0.32 0.35 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.phioffset),'HorizontalAlignment','right','Tag','phioffset','Visible','on','callback','bayes_callbacks(''phioffset'');');
+grasp_handles.user_modules.bayes.phioffset = uicontrol(figure_handle,'units','normalized','Position',[0.35 0.35 0.16 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.user_modules.bayes.phioffset),'HorizontalAlignment','right','Tag','phioffset','Visible','on','callback','bayes_callbacks(''phioffset'');');
 
 
 %rocktype
@@ -69,7 +69,7 @@ rock_type_list = {'san','phi'};
        end
    end
 uicontrol('units','normalized','Position',[0.053,0.25,0.4,0.08],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','rock type:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.rock_type = uicontrol('units','normalized','Position',[0.35,0.25,0.22,0.1],'HorizontalAlignment','center','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','Popup','Tag','rock_type',...
+grasp_handles.user_modules.bayes.rock_type = uicontrol('units','normalized','Position',[0.35,0.24,0.22,0.1],'HorizontalAlignment','center','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','Popup','Tag','rock_type',...
        'String',rock_type_list,'CallBack','bayes_callbacks(''rock_type'');','Value',value);
 
    
@@ -81,7 +81,7 @@ boxing_type_list = {'(none)','sectors','sector_boxes'};
        end
    end
 uicontrol('units','normalized','Position',[0.053,0.2,0.4,0.08],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','box type:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-grasp_handles.user_modules.bayes.boxing_type = uicontrol('units','normalized','Position',[0.35,0.2,0.35,0.1],'HorizontalAlignment','center','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','Popup','Tag','boxing_type',...
+grasp_handles.user_modules.bayes.boxing_type = uicontrol('units','normalized','Position',[0.35,0.19,0.35,0.1],'HorizontalAlignment','center','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','Popup','Tag','boxing_type',...
        'String',boxing_type_list,'CallBack','bayes_callbacks(''boxing_type'');','Value',value);   
 
 
@@ -106,6 +106,6 @@ grasp_handles.user_modules.bayes.nonsensefactor = uicontrol('units','normalized'
 
    
 %run bayes   
-grasp_handles.user_modules.bayes.run_bayes_button = uicontrol(figure_handle,'units','normalized','Position',[0.05 0.00 0.5 0.07],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','pushbutton','String','run_bayes!','HorizontalAlignment','Center','Tag','run_bayes_button','Visible','on','CallBack','bayes_callbacks(''run_bayes'');');
+grasp_handles.user_modules.bayes.run_bayes_button = uicontrol(figure_handle,'units','normalized','Position',[0.05 0.02 0.5 0.07],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','pushbutton','String','run_bayes!','HorizontalAlignment','Center','Tag','run_bayes_button','Visible','on','CallBack','bayes_callbacks(''run_bayes'');');
 
 bayes_callbacks;
